0f347b8769704088fce557497a99763453197582
Xyphrs <dromerof01@elpuig.xeill.net>
2021-03-17 20:05:29 +0100

    Initial commit

---
+++ src/Cabezones/Controller.java
@@ -0,0 +1,4 @@
+package Cabezones;
+
+public class Controller {
+}
---
+++ src/Cabezones/Main.java
@@ -0,0 +1,26 @@
+package Cabezones;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        String css = getClass().getResource("style.css").toExternalForm();
+        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
+        primaryStage.setTitle("Cabezones World Cup");
+        Scene scene = new Scene(root, 1345, 650);
+        scene.getStylesheets().add(css);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
